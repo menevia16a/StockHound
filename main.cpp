705393ee4024e9448487a23c4cf893b6b2a23999
menevia16a <veilbreaker@voidmaster.xyz>
2024-11-06 19:13:09 -0500

    Initial Commit
    Signed-off-by: menevia16a <menevia16a@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication application(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "StockHound_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            application.installTranslator(&translator);
+            break;
+        }
+    }
+
+    MainWindow window;
+
+    window.setWindowTitle("StockHound");
+    window.show();
+
+    return application.exec();
+}
